3af571f09e3457dc2628cc1b4babdd4e91dd126d
Evan Coleman <e@edc.me>
2022-02-08 19:09:28 -0500

    update coverage

--- fastlane/actions/code_coverage.rb
+++ fastlane/actions/code_coverage.rb
@@ -5,7 +5,7 @@ module Fastlane
         Actions.sh("curl -Os https://uploader.codecov.io/latest/macos/codecov")
 
         Actions.sh("chmod +x codecov")
-        Actions.sh("./codecov -f #{params[:file].shellescape} --build #{ENV["BITRISE_BUILD_NUMBER"]} --tag #{ENV["BITRISE_GIT_TAG"]} --branch #{ENV["BITRISE_GIT_BRANCH"]} --sha #{ENV["BITRISE_GIT_COMMIT"]} --token #{ENV["CODECOV_TOKEN"]}")
+        Actions.sh("./codecov -f #{params[:file].shellescape} --build #{ENV["BITRISE_BUILD_NUMBER"]} --pr #{ENV["BITRISE_PULL_REQUEST"]} --tag #{ENV["BITRISE_GIT_TAG"]} --branch #{ENV["BITRISE_GIT_BRANCH"]} --sha #{ENV["BITRISE_GIT_COMMIT"]} --token #{ENV["CODECOV_TOKEN"]}")
       end
 
       def self.description
